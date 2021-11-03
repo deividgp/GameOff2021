f8a8748a44900c1fb61e7f885e75fa6bcc4343c0
Deividgp <dayd4@outlook.com>
2021-11-03 14:03:13 +0100

    Project structure

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const app = express();
+const path = require('path');
+const port = 3000;
+
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ public/js/game.js
@@ -0,0 +1,58 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+const game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.setBaseURL('http://labs.phaser.io');
+    
+    // this.load.image('sky', 'assets/skies/space3.png');
+    // this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+    // this.load.image('red', 'assets/particles/red.png');
+    this.load.image('sky', 'assets/skies/sky1.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+}
+
+function create ()
+{
+    this.add.image(400, 300, 'sky');
+
+    const particles = this.add.particles('red');
+
+    const emitter = particles.createEmitter({
+        speed: 100,
+        scale: { start: 1, end: 0 },
+        blendMode: 'ADD'
+    });
+
+    const logo = this.physics.add.image(400, 100, 'logo');
+
+    logo.setVelocity(100, 200);
+    logo.setBounce(1, 1);
+    logo.setCollideWorldBounds(true);
+
+    emitter.startFollow(logo);
+}
+
+function update ()
+{
+
+}
