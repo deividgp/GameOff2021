3024a8480c6f7b9090c298ccb3039812c23e9bd3
Deividgp <dayd4@outlook.com>
2021-11-11 17:22:09 +0100

    Tutorial almost finished

--- public/js/game.js
+++ public/js/game.js
@@ -17,11 +17,12 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
+let player;
+let cursors;
 
-function preload ()
-{
+function preload() {
     this.load.setBaseURL('http://labs.phaser.io');
-    
+
     //this.load.image('sky', 'assets/skies/space3.png');
     this.load.image('logo', 'assets/sprites/phaser3-logo.png');
     this.load.image('red', 'assets/particles/red.png');
@@ -29,15 +30,16 @@ function preload ()
     this.load.image('ground', 'assets/sprites/platform.png');
     this.load.image('star', 'assets/sprites/star.png');
     this.load.image('bomb', 'assets/sprites/bomb.png');
+    this.load.spritesheet('dude',
+        'assets/sprites/dude.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
 }
 
-function create ()
-{
+function create() {
     this.add.image(400, 300, 'sky');
 
-    const particles = this.add.particles('red');
-
-    platforms = this.physics.add.staticGroup();
+    const platforms = this.physics.add.staticGroup();
 
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
@@ -45,22 +47,79 @@ function create ()
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
 
-    const emitter = particles.createEmitter({
-        speed: 100,
-        scale: { start: 1, end: 0 },
-        blendMode: 'ADD'
+    player = this.physics.add.sprite(100, 450, 'dude');
+    player.body.setGravityY(50);
+    //player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+    this.physics.add.collider(player, platforms);
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
     });
 
-    const logo = this.physics.add.image(400, 100, 'logo');
+    this.anims.create({
+        key: 'turn',
+        frames: [{ key: 'dude', frame: 4 }],
+        frameRate: 20
+    });
 
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
 
-    emitter.startFollow(logo);
+    cursors = this.input.keyboard.createCursorKeys();
+    
+    stars = this.physics.add.group({
+        key: 'star',
+        repeat: 11,
+        setXY: { x: 12, y: 0, stepX: 70 }
+    });
+    
+    stars.children.iterate(function (child) {
+    
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    
+    });
+    
+    // const particles = this.add.particles('red');
+    // const emitter = particles.createEmitter({
+    //     speed: 100,
+    //     scale: { start: 1, end: 0 },
+    //     blendMode: 'ADD'
+    // });
+
+    // const logo = this.physics.add.image(400, 100, 'logo');
+
+    // logo.setVelocity(100, 200);
+    // logo.setBounce(1, 1);
+    // logo.setCollideWorldBounds(true);
+
+    // emitter.startFollow(logo);
 }
 
-function update ()
-{
+function update() {
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
 
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    }
+    else {
+        player.setVelocityX(0);
+
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-330);
+    }
 }
