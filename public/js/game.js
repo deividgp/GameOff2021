879fd338afdebbf1abdb57772e4f2a8ba6bb7411
Deividgp <dayd4@outlook.com>
2021-11-12 13:54:09 +0100

    tutorial finished

--- public/js/game.js
+++ public/js/game.js
@@ -21,21 +21,17 @@ let player;
 let cursors;
 let platforms;
 let stars;
+let bombs;
 let score = 0;
 let scoreText;
 
 function preload() {
     this.load.image('star', 'assets/star.png');
     this.load.image('bomb', 'assets/bomb.png');
-    this.load.setBaseURL('http://labs.phaser.io');
-    //this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
-    this.load.image('sky', 'assets/skies/sky1.png');
-    this.load.image('ground', 'assets/sprites/platform.png');
-    
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
     this.load.spritesheet('dude',
-        'assets/sprites/dude.png',
+        'assets/dude.png',
         { frameWidth: 32, frameHeight: 48 }
     );
 }
@@ -43,53 +39,32 @@ function preload() {
 function create() {
     this.add.image(400, 300, 'sky');
     scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-    platforms = this.physics.add.staticGroup();
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-    platforms.create(600, 400, 'ground');
-    platforms.create(50, 250, 'ground');
-    platforms.create(750, 220, 'ground');
-
+    platformsBuilder(this);
     playerBuilder(this);
 
     cursors = this.input.keyboard.createCursorKeys();
 
-    stars = this.physics.add.group({
-        key: 'star',
-        repeat: 11,
-        setXY: { x: 12, y: 0, stepX: 70 }
-    });
+    starsBuilder(this);
+    
+    bombs = this.physics.add.group();
 
-    stars.children.iterate(function (child) {
-        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-    });
-
-    this.physics.add.overlap(player, stars, (player, star) => {
-        star.disableBody(true, true);
-        score += 10;
-        scoreText.setText('Score: ' + score);
-    }, null, this);
-
-    colliderBuilder(this);
-
-    // const particles = this.add.particles('red');
-    // const emitter = particles.createEmitter({
-    //     speed: 100,
-    //     scale: { start: 1, end: 0 },
-    //     blendMode: 'ADD'
-    // });
-
-    // const logo = this.physics.add.image(400, 100, 'logo');
-
-    // logo.setVelocity(100, 200);
-    // logo.setBounce(1, 1);
-    // logo.setCollideWorldBounds(true);
-
-    // emitter.startFollow(logo);
+    colliderOverlapBuilder(this);
 }
 
-function colliderBuilder(game) {
+function colliderOverlapBuilder(game) {
     game.physics.add.collider(player, platforms);
     game.physics.add.collider(stars, platforms);
+    game.physics.add.collider(bombs, platforms);
+    game.physics.add.collider(player, bombs, hitBomb, null, this);
+    game.physics.add.overlap(player, stars, collectStar, null, this);
+}
+
+function platformsBuilder(game){
+    platforms = game.physics.add.staticGroup();
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 }
 
 function playerBuilder(game) {
@@ -117,6 +92,46 @@ function playerBuilder(game) {
         frameRate: 10,
         repeat: -1
     });
+}
+
+function starsBuilder(game){
+    stars = game.physics.add.group({
+        key: 'star',
+        repeat: 11,
+        setXY: { x: 12, y: 0, stepX: 70 }
+    });
+
+    stars.children.iterate(function (child) {
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
+}
+
+function collectStar(player, star) {
+    star.disableBody(true, true);
+
+    score += 10;
+    scoreText.setText('Score: ' + score);
+
+    if (stars.countActive(true) === 0) {
+        stars.children.iterate(function (child) {
+            child.enableBody(true, child.x, 0, true, true);
+        });
+
+        let x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+        let bomb = bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    }
+}
+
+function hitBomb (player, bomb)
+{
+    this.physics.pause();
+    player.setTint(0xff0000);
+    player.anims.play('turn');
+    //gameOver = true;
 }
 
 function update() {
