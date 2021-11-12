d7ef28e4cf2d0853e9f3b851aed127b1d5fe1fb3
deividgp <dayd4@outlook.com>
2021-11-12 10:55:13 +0100

    More learning

--- public/js/game.js
+++ public/js/game.js
@@ -19,17 +19,21 @@ const config = {
 const game = new Phaser.Game(config);
 let player;
 let cursors;
+let platforms;
+let stars;
+let score = 0;
+let scoreText;
 
 function preload() {
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
     this.load.setBaseURL('http://labs.phaser.io');
-
     //this.load.image('sky', 'assets/skies/space3.png');
     this.load.image('logo', 'assets/sprites/phaser3-logo.png');
     this.load.image('red', 'assets/particles/red.png');
     this.load.image('sky', 'assets/skies/sky1.png');
     this.load.image('ground', 'assets/sprites/platform.png');
-    this.load.image('star', 'assets/sprites/star.png');
-    this.load.image('bomb', 'assets/sprites/bomb.png');
+    
     this.load.spritesheet('dude',
         'assets/sprites/dude.png',
         { frameWidth: 32, frameHeight: 48 }
@@ -38,54 +42,35 @@ function preload() {
 
 function create() {
     this.add.image(400, 300, 'sky');
-
-    const platforms = this.physics.add.staticGroup();
-
+    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+    platforms = this.physics.add.staticGroup();
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
 
-    player = this.physics.add.sprite(100, 450, 'dude');
-    player.body.setGravityY(50);
-    //player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
-    this.physics.add.collider(player, platforms);
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: 'turn',
-        frames: [{ key: 'dude', frame: 4 }],
-        frameRate: 20
-    });
-
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-        frameRate: 10,
-        repeat: -1
-    });
+    playerBuilder(this);
 
     cursors = this.input.keyboard.createCursorKeys();
-    
+
     stars = this.physics.add.group({
         key: 'star',
         repeat: 11,
         setXY: { x: 12, y: 0, stepX: 70 }
     });
-    
+
     stars.children.iterate(function (child) {
-    
         child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-    
     });
-    
+
+    this.physics.add.overlap(player, stars, (player, star) => {
+        star.disableBody(true, true);
+        score += 10;
+        scoreText.setText('Score: ' + score);
+    }, null, this);
+
+    colliderBuilder(this);
+
     // const particles = this.add.particles('red');
     // const emitter = particles.createEmitter({
     //     speed: 100,
@@ -102,6 +87,38 @@ function create() {
     // emitter.startFollow(logo);
 }
 
+function colliderBuilder(game) {
+    game.physics.add.collider(player, platforms);
+    game.physics.add.collider(stars, platforms);
+}
+
+function playerBuilder(game) {
+    player = game.physics.add.sprite(100, 450, 'dude');
+    player.body.setGravityY(50);
+    //player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    game.anims.create({
+        key: 'left',
+        frames: game.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    game.anims.create({
+        key: 'turn',
+        frames: [{ key: 'dude', frame: 4 }],
+        frameRate: 20
+    });
+
+    game.anims.create({
+        key: 'right',
+        frames: game.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+}
+
 function update() {
     if (cursors.left.isDown) {
         player.setVelocityX(-160);
@@ -115,7 +132,6 @@ function update() {
     }
     else {
         player.setVelocityX(0);
-
         player.anims.play('turn');
     }
 
